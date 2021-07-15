a812ad083bca1e30dd2e63f2f1f650b60963c304
bbb651 <bar.ye651@gmail.com>
2021-07-16 00:43:53 +0300

    Created empty plugin with needed dependencies

---
+++ NumericStats/NumericStats.cs
@@ -0,0 +1,18 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using UnboundLib;
+using UnityEngine;
+
+namespace NumericStats
+{
+    [BepInDependency("com.willis.rounds.unbound")]
+    [BepInPlugin("com.bbb651.rounds.numericstats", "NumericStats", "0.1.0")]
+    [BepInProcess("Rounds.exe")]
+    public class NumericStats : BaseUnityPlugin
+    {
+        private void Awake()
+        {
+            Logger.Log(LogLevel.Info, "Hello world from NumericStats!");
+        }
+    }
+}
